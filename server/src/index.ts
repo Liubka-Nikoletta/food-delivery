f47d28b0a04d98412f9d80722677b505f5c5a2e2
Nikoletta <nikolettaliubka@gmail.com>
2026-03-29 20:14:03 +0300

    feat: add database connection logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 dotenv.config();
 
@@ -8,5 +9,6 @@ const app = express();
 
 app.use(express.json());
 
+connectDB();
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
