680dd800adcda5d20bd789600a59e09824536745
Nikoletta <nikolettaliubka@gmail.com>
2026-03-31 23:03:34 +0300

    fix api.ts

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    timeout: 5000,
-})
+    baseURL: 'https://food-delivery-qcwz.onrender.com/api',
+    timeout: 5000
+});
 
 api.interceptors.response.use(
     (response) => response,
