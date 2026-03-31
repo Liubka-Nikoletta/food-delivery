0ab9fc3b7029b9a184919ede22cf7d6a9bb62b07
Nikoletta <nikolettaliubka@gmail.com>
2026-03-31 22:14:04 +0300

    fix: index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,8 +7,15 @@ import productRoutes from "./routes/productRoutes.js";
 import couponRoutes from "./routes/couponRoutes.js";
 import {encodeId} from "./utils/hashid.js";
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const clientPath = path.join(__dirname, '../../client/dist');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -25,6 +32,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
+
 app.use('/api', orderRoutes);
 app.use('/api', shopRoutes);
 app.use('/api', productRoutes);
@@ -34,6 +42,12 @@ app.get('/api/utils/encode/:id', (req, res) => {
     const { id } = req.params;
     const hash = encodeId(id);
     res.json({ hash });
+});
+
+app.use(express.static(clientPath));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(clientPath, 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
